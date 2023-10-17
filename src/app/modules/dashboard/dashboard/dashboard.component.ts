9252924bc55ae470ab2a16bf30104f48f9533371
Iman <inaseri.20@gmail.com>
2023-10-17 20:45:13 +0330

    fix: ui in auth

--- src/app/modules/dashboard/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {API} from "aws-amplify";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  jobs: any;
+
+  ngOnInit(): void {
+    this.getJobs()
+  }
+
+  getJobs() {
+    API.get('job', '/job', {}).then(result => {
+      this.jobs = result;
+    });
+  }
 
 }
